145feb9c49b20cf0bc107ccb1f463aa05324a4b4
Arturo <arturogregori836@gmail.com>
2026-03-11 12:25:39 +0100

    Pequeños arreglos

--- src/jcolonia/daw2025/tablasmvc/LanzadorTablaMultiplicar.java
+++ src/jcolonia/daw2025/tablasmvc/LanzadorTablaMultiplicar.java
@@ -8,11 +8,11 @@ public class LanzadorTablaMultiplicar {
 	* Inicia el menú principal del programa.
 	* @param argumentos opciones de ejecución -no se usan-
 	*/
-	public static void main (String[] argumentos){
+	static void main (String[] argumentos){
 		ControlTablaMultiplicar programa;
 		
 		programa = new ControlTablaMultiplicar();
 		
-		programa.bucleprincipal();
+		programa.buclePrincipal();
 	}
 }
